2f47102ba8dd2a524700be01bce8de9858e36b84
rijalsawan <rijalsawan@gmail.com>
2023-07-27 19:45:46 -0500

    4

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./input.css";
+import Navbar from './components/Navbar';
+import Body from './components/Body';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Body/>
+    <Contact/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
